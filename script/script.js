152bacd009f0efc491b911cc76543989f585a7ab
Zihad-1883 <your@email.com>
2026-03-20 21:10:55 +0600

    done for now

--- script/script.js
+++ script/script.js
@@ -6,6 +6,16 @@ const loadLevels = () => {
  
 loadLevels()
 
+function removeActive(){
+    const allBtn = document.querySelectorAll('.lesson-btn')
+    // console.log(allBtn);
+    allBtn.forEach(btn => {
+        // console.log(btn)
+        btn.classList.remove('active')
+    })
+    
+}
+
 const displayLevel = (lessons) => {
     const levelContainer = document.getElementById('level-container');
     // console.log(lessons.data);
@@ -13,7 +23,7 @@ const displayLevel = (lessons) => {
         // console.log(level)
         const levelBtn = document.createElement('button');
         levelBtn.innerHTML = `
-            <button onclick = "loadWords(${level.level_no})" class="btn btn-outline btn-primary"><img src="./assets/fa-book-open.png" alt="">Lesson - ${level.level_no}</button>
+            <button onclick = "loadWords(${level.level_no})" id = "lesson-btn-${level.level_no}" class="btn btn-outline btn-primary lesson-btn"><img src="./assets/fa-book-open.png" alt="">Lesson - ${level.level_no}</button>
         `
         levelContainer.appendChild(levelBtn)
     });
@@ -21,9 +31,17 @@ const displayLevel = (lessons) => {
 }
 
 const loadWords = (id) => {
+    manageLoading(true)
     const res = fetch(`https://openapi.programming-hero.com/api/level/${id}`)
         .then(res => res.json())
-        .then(data => displayWords(data))
+        .then(data => {
+            const clickedBtn = document.getElementById(`lesson-btn-${id}`)
+            removeActive()
+            clickedBtn.classList.add('active')
+            displayWords(data.data)
+            // console.log(data.data)
+            manageLoading(false)
+        })
 }
 
 const displayWords = (words) => {
@@ -31,7 +49,8 @@ const displayWords = (words) => {
     const wordsContainer = document.getElementById('words-container');
     wordsContainer.innerHTML = ``;
 
-    if(words.data.length === 0){
+    console.log(words)
+    if(words.length === 0){
         wordsContainer.innerHTML = `
             <div class="space-y-3 flex flex-col items-center p-5 col-span-full">
                 <img src = "./assets/alert-error.png">
@@ -41,9 +60,9 @@ const displayWords = (words) => {
         `
     }
 
-    words.data.forEach(word => {
+    words.forEach(word => {
         // console.log(word.meaning);
-        
+        // console.log(word.id)
         const wordDiv = document.createElement('div');
         wordDiv.innerHTML = `
             <div class="rounded-md p-13 bg-white">
@@ -53,7 +72,7 @@ const displayWords = (words) => {
                     <h2 class="font-bangla text-2xl font-semibold">${word.meaning ? word.meaning : "Meaning not found"} / ${word.pronunciation ? word.pronunciation : "Pronounciation not found"}"</h2>
                 </div>
                 <div class="flex justify-between items-center">
-                    <img src="./assets/Vector.png" alt=""> 
+                    <img onclick="loadWordDetails(${word.id})" src="./assets/Vector.png" alt=""> 
                     <img src="./assets/fi-sr-volume.png" alt="">
                 </div>
             </div>
@@ -62,3 +81,74 @@ const displayWords = (words) => {
     })
     
 }
+
+
+function loadWordDetails(id){
+    const res = fetch(`https://openapi.programming-hero.com/api/word/${id}`)
+        .then(res => res.json())
+        .then(data => displayWordDetails(data))
+}
+
+function displayWordDetails(clickedWord){
+    console.log(clickedWord.data)
+    const wordDetails = document.getElementById('word-details');
+    wordDetails.innerHTML = ""
+    const wordDetailsDiv = document.createElement('div');
+    wordDetailsDiv.innerHTML = `
+        <div class="space-y-8 p-5">
+                <div>
+                    <h2 class="text-[36px] font-semibold">${clickedWord.data.word} (${clickedWord.data.pronunciation})</h2>
+                </div>
+                <div class="space-y-3">
+                    <h5 class="text-2xl font-semibold">Meaning</h5>
+                    <p class="text-xl font-medium font-bangla">${clickedWord.data.meaning}</p>
+                </div>
+                <div class="space-y-3">
+                    <h5 class="text-2xl font-semibold">Example</h5>
+                    <p class="text-xl font-medium">${clickedWord.data.sentence}</p>
+                </div>
+                <div class="space-y-3">
+                    <h5 class="text-2xl font-medium font-bangla">সমার্থক শব্দ গুলো</h5>
+                    <span class="text-xl bg-[#dcefff] px-3 py-2 mr-1">${clickedWord.data.synonyms[0]}</span>
+                    <span class="text-xl bg-[#dcefff] px-3 py-2 mr-1">${clickedWord.data.synonyms[1]}</span>
+                    <span class="text-xl bg-[#dcefff] px-3 py-2">${clickedWord.data.synonyms[2]}</span>
+                </div>
+        </div>            
+    
+               
+    ` 
+    wordDetails.appendChild(wordDetailsDiv)
+    my_modal_5.showModal()
+}
+ 
+
+function manageLoading(status){
+    const loadingSection = document.getElementById('loadingSection');
+    if(status === true){
+        loadingSection.classList.remove('hidden');
+        document.getElementById('words-container').classList.add('hidden');
+    }
+    else if(status === false){
+        loadingSection.classList.add('hidden');
+        document.getElementById('words-container').classList.remove('hidden');
+    }
+}
+
+
+// search functionality
+document.getElementById('search-btn').addEventListener('click' , () => {
+    const searchInput = document.getElementById('search-input')
+    const searchValue = searchInput.value.trim().toLowerCase()
+    console.log(searchValue)
+
+    removeActive()
+    const res = fetch('https://openapi.programming-hero.com/api/words/all')
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data.data);
+            const allWords = data.data;
+            const filterWords = allWords.filter(word => word.word.toLowerCase().includes(searchValue));
+            console.log(filterWords)
+            displayWords(filterWords)
+        })
+})
